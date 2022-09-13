e3f17874d000c2cba11ea4a13e6b90933cf69576
batimovil <german.figueroa@ort.edu.ar>
2022-09-13 20:19:22 -0300

    hero files added

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,9 +3,6 @@ import React from 'react'
 import styles from './hero.module.css';
 
 const Hero = () => {
-  return (
-    <div className={ styles['hero'] }>Hero</div>
-  )
-}
-
-export default Hero
+  return <div className={styles['hero']}>Hero</div>;
+};
+export default Hero;
