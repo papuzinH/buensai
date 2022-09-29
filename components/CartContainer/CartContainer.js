dfec1eaa8904113eca928ab995dc2a3ed39ad883
cdejtiar <cdejtiar14@gmail.com>
2022-09-29 20:27:21 -0300

    imgs added

--- components/CartContainer/CartContainer.js
+++ components/CartContainer/CartContainer.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import styles from './cartcontainer.module.css';
 import CartItem from '../CartItem/CartItem';
+import OlmoChino from '../../public/assets/olmoChino.png';
+import Shito from '../../public/assets/shito.png';
+import Shohin from '../../public/assets/shohin.png';
 
 const CartContainer = () => {
   const dataCart = [
     {
-      //imagen: OlmoChino,
+      imagen: OlmoChino,
       nombre: 'Olmo Chino',
       precio: 4500,
       id: '1',
     },
     {
-      //imagen: Shito,
+      imagen: Shito,
       nombre: 'Shito',
       precio: 5000,
       id: '2',
     },
     {
-      //imagen: Shohin,
+      imagen: Shohin,
       nombre: 'Shohin',
       precio: 3800,
       id: '3',
@@ -28,13 +31,15 @@ const CartContainer = () => {
     <div className={`inner`}>
       <h2 className={`${styles['title']}`}>Carrito</h2>
       {dataCart.map((item) => {
-        {
-          total = total + item.precio;
-        }
+        total = total + item.precio;
         return (
-          <>
-            <CartItem /*image{item.imagen}*/ name={item.nombre} price={item.precio} key={item.id} />
-          </>
+          <CartItem
+            key={item.id}
+            indice={item.id}
+            image={item.imagen}
+            name={item.nombre}
+            price={item.precio}
+          />
         );
       })}
       <h2 className={`${styles['total']}`}>
