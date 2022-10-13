32de1d10e33f39fc2f417a258d8c3dc05b73891e
MaxiCo1 <coletti.maxi@gmail.com>
2022-10-13 19:54:58 -0300

    loader denuevo

--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -4,9 +4,9 @@ import styles from './loader.module.css';
 const Loader = () => {
   return (
     <div className={`${styles['loader']}`}>
-      <div className={`${styles['ball']}`}>Loader</div>
-      <div className={`${styles['ball']}`}>Loader</div>
-      <div className={`${styles['ball']}`}>Loader</div>
+      <div className={`${styles['ball']}`}></div>
+      <div className={`${styles['ball']}`}></div>
+      <div className={`${styles['ball']}`}></div>
       <span>Loading...</span>
     </div>
   );
